26364d67627db0c227cea116699485fb33bc691e
leoncoderehab <leon@code.rehab>
2016-09-29 15:20:30 +0200

    init

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+if(jQuery){
+
+	var onWindowResize = function(args){
+		if(jQuery(window).innerWidth() < 782){
+			jQuery(args.menu_toggle_button).show();
+		}else{
+			jQuery(args.menu_selector).removeClass('visible')
+			jQuery(args.menu_toggle_button).hide();
+		}
+
+	}
+
+	function initMenu(args){
+		args = args || {
+			menu_selector: "#menu-main-menu",
+			menu_toggle_button: ".toggle-menu-button"
+		};
+
+		jQuery(window).resize(function(e){
+			onWindowResize(args);
+		})
+		onWindowResize(args);
+
+		jQuery(args.menu_toggle_button).click(function(e){
+					alert('hallo');
+			jQuery(args.menu_selector).toggleClass('visible')
+		});
+	}
+}
+
+jQuery( document ).ready(function() {
+  initMenu();
+});
